6e442215663ae387023bb412f0e6a5327e0419a3
felipe.ferreira <felipe.ferreira@corp.globo.com>
2020-05-26 14:30:40 -0300

    updated model in order to perform model training step inside ai-platform

---
+++ logo-classification/setup.py
@@ -0,0 +1,21 @@
+'''Cloud ML Engine package configuration.'''
+from setuptools import setup, find_packages
+
+with open("version") as version_file:
+    version = version_file.read().strip()
+
+
+setup(name='logo_classification_model',
+      version=version,
+      packages=find_packages(),
+      include_package_data=True,
+      description='Logo classification model on Cloud ML Engine',
+      author='Felipe Ferreira',
+      author_email='felipe.ferreira@corp.globo.com',
+      license='Unlicense',
+      install_requires=[
+          'tensorflow-gpu==2.1.0',
+          'setuptools==46.4.0',
+          'pillow==7.1.2',
+          'scikit-learn==0.23.1'],
+      zip_safe=False)
